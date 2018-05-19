0da957a4409ddfab412bb5dc4723d17dc189dc18
Bink Nam <twitcht.maxvolt@gmail.com>
2018-05-19 13:43:05 +0700

    delete duplicate file

--- Invictus-master/SQL/CreateTableUsers.sql
+++
@@ -1,9 +0,0 @@
-use invictus;
-create table USERS (
-	Id bigint identity(1,1) primary key,
-	Username varchar(255),
-	Password varchar(255),
-	Name varchar(255)
-);
-insert into Users(username, password, name) values ('invictus@gmail.com', 'admin123', 'Invictus');
-select * from Users;
