5fb12bf9b103a8de45a396f96fc7ee92c7077faa
Bink Nam <twitcht.maxvolt@gmail.com>
2018-05-19 22:24:31 +0700

    update login

--- Invictus/Form1.cs
+++ Invictus/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Form1
 {
     public partial class Frdangnhap : Form
     {
+        SqlConnection sqlConnection = new SqlConnection(
+            @"Data Source=INVICTUS-PC\SQLEXPRESS;Initial Catalog=invictus;Integrated Security=True");
         public Frdangnhap()
         {
             InitializeComponent();
@@ -19,6 +22,7 @@ namespace Form1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -43,9 +47,31 @@ namespace Form1
 
         private void bntdangnhap_Click(object sender, EventArgs e)
         {
-            if(txtusername.Text != String.Empty && txtpass.Text!= String.Empty)
+            if (txtusername.Text != String.Empty && txtpass.Text!= String.Empty)
             {
-                
+                var dap = new SqlDataAdapter("select * from dbo.users where dbo.users.username = @username", sqlConnection);
+                dap.SelectCommand.Parameters.AddWithValue("@username", txtusername.Text);
+
+                var table = new DataTable();
+                dap.Fill(table);
+                if (table.Rows.Count == 1)
+                {
+                    foreach (DataRow row in table.Rows)
+                    {
+                        if ((String)row["password"] == txtpass.Text)
+                        {
+                            grbdangnhap.Text = "Dang nhap thanh cong";
+                        }
+                        else
+                        {
+                            grbdangnhap.Text = "Sai Pass";
+                        }
+                    }
+                }
+                else
+                {
+                    grbdangnhap.Text = "Sai ten tai khoan!";
+                }
             }
         }
 
