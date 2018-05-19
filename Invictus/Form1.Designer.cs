8ce45e9e904c8e2c88ed7c7c73b8127970c99ec3
HoaiNam <lehoainam295@gmail.com>
2018-05-19 14:09:14 +0700

    Signed-off-by: HoaiNam <lehoainam295@gmail.com>

--- Invictus/Form1.Designer.cs
+++ Invictus/Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Invictus
+﻿namespace Form1
 {
-    partial class Form1
+    partial class Frdangnhap
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grbdangnhap = new System.Windows.Forms.GroupBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bntdangnhap = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.grbdangnhap.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbdangnhap
+            // 
+            this.grbdangnhap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.grbdangnhap.Controls.Add(this.txtpass);
+            this.grbdangnhap.Controls.Add(this.txtusername);
+            this.grbdangnhap.Controls.Add(this.label2);
+            this.grbdangnhap.Controls.Add(this.label1);
+            this.grbdangnhap.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbdangnhap.ForeColor = System.Drawing.Color.Red;
+            this.grbdangnhap.ImeMode = System.Windows.Forms.ImeMode.Close;
+            this.grbdangnhap.Location = new System.Drawing.Point(39, 34);
+            this.grbdangnhap.Name = "grbdangnhap";
+            this.grbdangnhap.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.grbdangnhap.Size = new System.Drawing.Size(339, 168);
+            this.grbdangnhap.TabIndex = 0;
+            this.grbdangnhap.TabStop = false;
+            this.grbdangnhap.Text = "Đăng Nhập Hệ Thống";
+            this.grbdangnhap.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtpass
+            // 
+            this.txtpass.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtpass.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txtpass.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.Location = new System.Drawing.Point(121, 96);
+            this.txtpass.Multiline = true;
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(127, 25);
+            this.txtpass.TabIndex = 3;
+            this.txtpass.TextChanged += new System.EventHandler(this.txtpass_TextChanged);
+            // 
+            // txtusername
+            // 
+            this.txtusername.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtusername.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txtusername.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Location = new System.Drawing.Point(121, 49);
+            this.txtusername.Multiline = true;
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(127, 25);
+            this.txtusername.TabIndex = 2;
+            this.txtusername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(23, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PassWord";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(23, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // bntdangnhap
+            // 
+            this.bntdangnhap.BackColor = System.Drawing.Color.Gainsboro;
+            this.bntdangnhap.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bntdangnhap.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntdangnhap.ForeColor = System.Drawing.Color.Black;
+            this.bntdangnhap.Location = new System.Drawing.Point(66, 219);
+            this.bntdangnhap.Name = "bntdangnhap";
+            this.bntdangnhap.Size = new System.Drawing.Size(85, 27);
+            this.bntdangnhap.TabIndex = 1;
+            this.bntdangnhap.Text = "Đăng Nhập";
+            this.bntdangnhap.UseVisualStyleBackColor = false;
+            this.bntdangnhap.Click += new System.EventHandler(this.bntdangnhap_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Black;
+            this.btnExit.Location = new System.Drawing.Point(293, 219);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(85, 27);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Frdangnhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(422, 281);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.bntdangnhap);
+            this.Controls.Add(this.grbdangnhap);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Frdangnhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Nhập";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grbdangnhap.ResumeLayout(false);
+            this.grbdangnhap.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grbdangnhap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.Button bntdangnhap;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- Invictus/Form1.cs
+++ Invictus/Form1.cs
@@ -8,13 +8,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Invictus
+namespace Form1
 {
-    public partial class Form1 : Form
+    public partial class Frdangnhap : Form
     {
-        public Form1()
+        public Frdangnhap()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntdangnhap_Click(object sender, EventArgs e)
+        {
+            if(txtusername.Text != String.Empty && txtpass.Text!= String.Empty)
+            {
+                
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
